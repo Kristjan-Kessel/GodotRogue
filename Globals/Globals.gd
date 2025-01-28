95ff8d73110e4c7961e80c2b8865079cae2529ad
kristjan-kessel <79764375+Kristjan-Kessel@users.noreply.github.com>
2025-01-28 17:17:53 +0200

    Init

---
+++ Globals/Globals.gd
@@ -0,0 +1,5 @@
+extends Node
+
+# Global variables
+var map_width: int = 80   # Width of the dungeon map
+var map_height: int = 24  # Height of the dungeon map
---
+++ Scripts/main.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _ready():
+	print("Map Size: ", Globals.map_width, "x", Globals.map_height)
