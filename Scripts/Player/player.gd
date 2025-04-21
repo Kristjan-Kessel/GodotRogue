3b8ced86459a0a977dddb34b9d5dcac93aa13ea8
kristjan-kessel <79764375+Kristjan-Kessel@users.noreply.github.com>
2025-04-21 19:48:41 +0300

    Use match case instead of if else for player commands

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -40,121 +40,121 @@ func _process(delta: float) -> void:
 		action_delay_timer -= delta
 		return
 	
-	if current_command == CommandType.DROP:
-		if Input.is_action_just_pressed("view_options"):
-			open_inventory.emit("- Select an item to drop -")
-		else:	
-			var item = get_item_from_key()
-			if item != null:
-				drop_item.emit(item)
-		return
-	elif current_command == CommandType.USE_ITEM:
-		if Input.is_action_just_pressed("view_options"):
-			open_inventory.emit("- Select an item to use -")
-		else:	
-			var item = get_item_from_key()
-			if item != null:
-				if item.type == item.Type.USEABLE:
-					item.on_use(self)
-					inventory.erase(item)
-					log_message.emit("Used "+item.label)
-				else:
-					log_message.emit("Invalid item.")
-					current_command = CommandType.NONE
-				render_map.emit()
-		return
-		return
-	elif current_command == CommandType.WEAR_ARMOR:
-		if Input.is_action_just_pressed("view_options"):
-			open_inventory.emit("- Select an item to wear as armor -")
-		else:	
-			var item = get_item_from_key()
-			if item != null:
-				if item.type == item.Type.ARMOR:
-					if armor_item != null:
-						inventory.append(armor_item)
-					armor_item = item
-					stats.bonus_armor = armor_item.armor_bonus
-					inventory.erase(item)
-					log_message.emit("Wore "+item.label+" as armor.")
-				else:
-					log_message.emit("Invalid item.")
-					current_command = CommandType.NONE
-				render_map.emit()
-		return
-	elif current_command == CommandType.WIELD_WEAPON:
-		if Input.is_action_just_pressed("view_options"):
-			open_inventory.emit("- Select an item to wield as a weapon -")
-		else:	
-			var item = get_item_from_key()
-			if item != null:
-				if item.type == item.Type.WEAPON:
-					if weapon_item != null:
-						inventory.append(weapon_item)
-					weapon_item = item
-					inventory.erase(item)
-					log_message.emit("Wielded "+item.label+" as a weapon.")
-				else:
-					log_message.emit("Invalid item.")
-					current_command = CommandType.NONE
-				render_map.emit()
-		return
-	elif current_command == CommandType.INVENTORY:
-		if Input.is_action_just_pressed("continue"):
-			current_command = CommandType.NONE
-			render_map.emit()
-	elif current_command == CommandType.MOVE:
-		var new_direction = Vector2.ZERO
-	
-		if Input.is_action_pressed("ui_up"):
-			new_direction = Vector2(0, -1)
-		elif Input.is_action_pressed("ui_down"):
-			new_direction = Vector2(0, 1)
-		elif Input.is_action_pressed("ui_left"):
-			new_direction = Vector2(-1, 0)
-		elif Input.is_action_pressed("ui_right"):
-			new_direction = Vector2(1, 0)
-
-		# Lets you move one tile at a time, or keep moving if you keep holding the key.
-		if new_direction != Vector2.ZERO:
-			if !is_moving || new_direction != move_direction:
-				move_direction = new_direction
-				is_moving = true
-				move_delay_timer = INITIAL_MOVE_DELAY
-				move_repeat_timer = 0.0
-				move_player()
-			elif is_moving:
-				move_delay_timer -= delta
-				if move_delay_timer <= 0:
-					move_repeat_timer -= delta
-					if move_repeat_timer <= 0:
-						move_player()
-						move_repeat_timer = REPEAT_RATE
-		elif is_moving:
-			is_moving = false
-	elif current_command == CommandType.FIND:
-		var direction = Vector2.ZERO
-		if Input.is_action_just_pressed("ui_up"):
-			direction = Vector2(0, -1)
-		elif Input.is_action_just_pressed("ui_down"):
-			direction = Vector2(0, 1)
-		elif Input.is_action_just_pressed("ui_left"):
-			direction = Vector2(-1, 0)
-		elif Input.is_action_just_pressed("ui_right"):
-			direction = Vector2(1, 0)
-		elif Input.is_action_just_pressed("command_find"):
-			current_command = CommandType.NONE
-			log_message.emit("")
-
-		if direction != Vector2.ZERO:
-			command_find.emit(direction)
-			clear_command()
-	elif current_command == CommandType.ATTACK:
-		if Input.is_action_just_pressed("continue"):
-			current_command = CommandType.MOVE
-			log_message.emit("")
-			action_taken.emit()
+	match current_command:
+		CommandType.DROP:
+			if Input.is_action_just_pressed("view_options"):
+				open_inventory.emit("- Select an item to drop -")
+			else:	
+				var item = get_item_from_key()
+				if item != null:
+					drop_item.emit(item)
 			return
+		CommandType.USE_ITEM:
+			if Input.is_action_just_pressed("view_options"):
+				open_inventory.emit("- Select an item to use -")
+			else:	
+				var item = get_item_from_key()
+				if item != null:
+					if item.type == item.Type.USEABLE:
+						item.on_use(self)
+						inventory.erase(item)
+						log_message.emit("Used "+item.label)
+					else:
+						log_message.emit("Invalid item.")
+						current_command = CommandType.NONE
+					render_map.emit()
+			return
+		CommandType.WEAR_ARMOR:
+			if Input.is_action_just_pressed("view_options"):
+				open_inventory.emit("- Select an item to wear as armor -")
+			else:	
+				var item = get_item_from_key()
+				if item != null:
+					if item.type == item.Type.ARMOR:
+						if armor_item != null:
+							inventory.append(armor_item)
+						armor_item = item
+						stats.bonus_armor = armor_item.armor_bonus
+						inventory.erase(item)
+						log_message.emit("Wore "+item.label+" as armor.")
+					else:
+						log_message.emit("Invalid item.")
+						current_command = CommandType.NONE
+					render_map.emit()
+			return
+		CommandType.WIELD_WEAPON:
+			if Input.is_action_just_pressed("view_options"):
+				open_inventory.emit("- Select an item to wield as a weapon -")
+			else:	
+				var item = get_item_from_key()
+				if item != null:
+					if item.type == item.Type.WEAPON:
+						if weapon_item != null:
+							inventory.append(weapon_item)
+						weapon_item = item
+						inventory.erase(item)
+						log_message.emit("Wielded "+item.label+" as a weapon.")
+					else:
+						log_message.emit("Invalid item.")
+						current_command = CommandType.NONE
+					render_map.emit()
+			return
+		CommandType.INVENTORY:
+			if Input.is_action_just_pressed("continue"):
+				current_command = CommandType.NONE
+				render_map.emit()
+		CommandType.MOVE:
+			var new_direction = Vector2.ZERO
+		
+			if Input.is_action_pressed("ui_up"):
+				new_direction = Vector2(0, -1)
+			elif Input.is_action_pressed("ui_down"):
+				new_direction = Vector2(0, 1)
+			elif Input.is_action_pressed("ui_left"):
+				new_direction = Vector2(-1, 0)
+			elif Input.is_action_pressed("ui_right"):
+				new_direction = Vector2(1, 0)
+
+			# Lets you move one tile at a time, or keep moving if you keep holding the key.
+			if new_direction != Vector2.ZERO:
+				if !is_moving || new_direction != move_direction:
+					move_direction = new_direction
+					is_moving = true
+					move_delay_timer = INITIAL_MOVE_DELAY
+					move_repeat_timer = 0.0
+					move_player()
+				elif is_moving:
+					move_delay_timer -= delta
+					if move_delay_timer <= 0:
+						move_repeat_timer -= delta
+						if move_repeat_timer <= 0:
+							move_player()
+							move_repeat_timer = REPEAT_RATE
+			elif is_moving:
+				is_moving = false
+		CommandType.FIND:
+			var direction = Vector2.ZERO
+			if Input.is_action_just_pressed("ui_up"):
+				direction = Vector2(0, -1)
+			elif Input.is_action_just_pressed("ui_down"):
+				direction = Vector2(0, 1)
+			elif Input.is_action_just_pressed("ui_left"):
+				direction = Vector2(-1, 0)
+			elif Input.is_action_just_pressed("ui_right"):
+				direction = Vector2(1, 0)
+			elif Input.is_action_just_pressed("command_find"):
+				current_command = CommandType.NONE
+				log_message.emit("")
+
+			if direction != Vector2.ZERO:
+				command_find.emit(direction)
+				clear_command()
+		CommandType.ATTACK:
+			if Input.is_action_just_pressed("continue"):
+				current_command = CommandType.MOVE
+				log_message.emit("")
+				action_taken.emit()
+				return
 	if current_command == CommandType.NONE || current_command == CommandType.MOVE:
 		if Input.is_action_just_pressed("ui_up") || Input.is_action_just_pressed("ui_down") || Input.is_action_just_pressed("ui_left") || Input.is_action_just_pressed("ui_right"):  
 			current_command = CommandType.MOVE
